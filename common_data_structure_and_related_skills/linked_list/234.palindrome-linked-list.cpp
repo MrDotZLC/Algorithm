60baae81d5b656de703354cb37d0f2a9c3de7a50
MrDotZLC <zlc3986@163.com>
2025-08-24 14:49:21 +0800

    add 876 2095 234 2130 143 141 142 457 to linked_list

---
+++ common_data_structure_and_related_skills/linked_list/234.palindrome-linked-list.cpp
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode id=234 lang=cpp
+ *
+ * [234] Palindrome Linked List
+ *
+ * https://leetcode.com/problems/palindrome-linked-list/description/
+ *
+ * algorithms
+ * Easy (56.13%)
+ * Likes:    17680
+ * Dislikes: 948
+ * Total Accepted:    2.6M
+ * Total Submissions: 4.5M
+ * Testcase Example:  '[1,2,2,1]'
+ *
+ * Given the head of a singly linked list, return true if it is a palindrome or
+ * false otherwise.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: head = [1,2,2,1]
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: head = [1,2]
+ * Output: false
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the list is in the range [1, 10^5].
+ * 0 <= Node.val <= 9
+ * 
+ * 
+ * 
+ * Follow up: Could you do it in O(n) time and O(1) space?
+ */
+#include "template.h"
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode *mid = get_middle(head);
+        ListNode *head2 = reverse(mid);
+        while (head2) {
+            if (head->val != head2->val) {
+                return false;
+            }
+            head = head->next;
+            head2 = head2->next;
+        }
+        return true;
+    }
+
+    ListNode* reverse(ListNode* head) {
+        ListNode *cur = head, *pre = nullptr;
+        while (cur) {
+            ListNode *nxt = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = nxt;
+        }
+        return pre;
+    }
+
+    ListNode* get_middle(ListNode* head) {
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
+// @lc code=end
+
