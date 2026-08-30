835be96174aff56264fb12938042e1d015f1093b
MrDotZLC <zlc3986@163.com>
2026-08-31 03:44:09 +0800

    complate singleton

---
+++ interview_practice/design_patterns/singleton.cpp
@@ -0,0 +1,16 @@
+class Singleton {
+public:
+    static Singleton& get_instance() {
+        static Singleton instance;
+        return instance;
+    }
+
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
+    Singleton(Singleton&&) = delete;
+    Singleton& operator=(Singleton&&) = delete;
+
+private:
+    Singleton() = default;
+    ~Singleton() = default;
+};
